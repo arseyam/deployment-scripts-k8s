34f602fae4350ec1a54c13e27d7c6055a31e7d1c
Shaun Gautz <shaun.gautz@dynatrace.com>
2019-08-21 15:01:54 -0500

    Update create.sh

--- deployment_eT_K8s_demo/scripts/create.sh
+++ deployment_eT_K8s_demo/scripts/create.sh
@@ -3,6 +3,6 @@
 echo "Creating easyTravel"
 kubectl create -f easytravel-mongodb-deployment.yaml
 kubectl create -f easytravel-mongodb-service.yaml
-kubectl create -f easytravel-pluginservice-deployment.yaml
+#!kubectl create -f easytravel-pluginservice-deployment.yaml
 kubectl create -f easytravel-backend-deployment.yaml
 kubectl create -f easytravel-frontend-deployment.yaml
