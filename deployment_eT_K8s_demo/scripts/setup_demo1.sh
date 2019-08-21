2b94eae352a3dcaa9091d7a7221cc1625b84e5e1
Shaun Gautz <gautzsha@gmail.com>
2019-08-21 15:43:33 -0500

    updating all file

--- deployment_eT_K8s_demo/scripts/setup_demo1.sh
+++ deployment_eT_K8s_demo/scripts/setup_demo1.sh
@@ -2,4 +2,6 @@
 
 echo "Running setup for demo1"
 #!./setup_base.sh
+
+kubectl create namespace demo-live-1
 kubectl config set contexts.default-context.namespace demo-live-1
--- deployment_eT_K8s_demo/scripts/setup_demo2.sh
+++ deployment_eT_K8s_demo/scripts/setup_demo2.sh
@@ -2,4 +2,5 @@
 
 echo "Running setup for demo2"
 #!./setup_base.sh
+kubectl create namespace demo-live-2
 kubectl config set contexts.default-context.namespace demo-live-2
