fc8513e9cc8fe59c18387258d610963ba7ecc63f
Shaun Gautz <gautzsha@gmail.com>
2019-08-22 14:53:29 -0500

    all files

--- deployment_eT_K8s_demo/scripts/create_easytravel.sh
+++ deployment_eT_K8s_demo/scripts/create_easytravel.sh
@@ -10,10 +10,12 @@ kubectl create -f easytravel-mongodb-deployment.yaml
 kubectl create -f easytravel-mongodb-service.yaml
 kubectl create -f easytravel-backend-deployment.yaml
 kubectl create -f easytravel-frontend-deployment.yaml
+kubectl create -f easytravel-nginx-deployment.yaml
 
 echo "Exposing easyTravel"
 kubectl create -f easytravel-backend-service.yaml
 kubectl create -f easytravel-frontend-service.yaml
+kubectl create -f easytravel-nginx-service.yaml
 echo "Waiting for 150 seconds before starting loadgen"
 sleep 150
 kubectl create -f easytravel-loadgen-deployment.yaml
