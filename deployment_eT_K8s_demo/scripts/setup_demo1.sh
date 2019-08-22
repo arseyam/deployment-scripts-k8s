66d4acb9d81bca0789af8e49e5ccb3630a9676d4
Shaun Gautz <shaun.gautz@dynatrace.com>
2019-08-22 11:06:18 -0500

    Update and rename setup_demo1.sh to setup_easytravel.sh

--- deployment_eT_K8s_demo/scripts/setup_demo1.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-echo "Running setup for demo1"
-#!./setup_base.sh
-
-#!kubectl create namespace demo-live-1
-#!kubectl config use-context demo-live-1
-kubectl config set contexts.default-context.namespace demo-live-1
