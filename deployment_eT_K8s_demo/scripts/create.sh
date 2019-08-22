354fc4943fd62fdd8342b6bbf554b50e944a9178
Shaun Gautz <shaun.gautz@dynatrace.com>
2019-08-22 11:08:51 -0500

    Delete create.sh

--- deployment_eT_K8s_demo/scripts/create.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-echo "Creating easyTravel"
-kubectl create -f easytravel-mongodb-deployment.yaml
-kubectl create -f easytravel-mongodb-service.yaml
-kubectl create -f easytravel-backend-deployment.yaml
-kubectl create -f easytravel-frontend-deployment.yaml
