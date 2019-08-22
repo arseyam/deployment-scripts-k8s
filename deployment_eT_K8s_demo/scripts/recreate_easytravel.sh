55b2b3b98713f78d7b962e03781d9149bf4a3df1
Shaun Gautz <shaun.gautz@dynatrace.com>
2019-08-22 11:16:21 -0500

    Update recreate_easytravel.sh

--- deployment_eT_K8s_demo/scripts/recreate_easytravel.sh
+++ deployment_eT_K8s_demo/scripts/recreate_easytravel.sh
@@ -1,16 +1,7 @@
 #!/bin/sh
 
 echo "Recreating easyTravel"
-./setup_demo1.sh
-./delete.sh
-kubectl delete -f easytravel-loadgen-deployment.yaml
-kubectl delete -f easytravel-frontend-service.yaml
-kubectl delete -f easytravel-backend-service.yaml
+./delete_easytravel.sh
 echo "Waiting for 120 seconds"
 sleep 120
-./create.sh
-kubectl create -f easytravel-backend-service.yaml
-kubectl create -f easytravel-frontend-service.yaml
-echo "Waiting for 150 seconds before starting loadgen"
-sleep 150
-kubectl create -f easytravel-loadgen-deployment.yaml
+./create_easytravel.sh
