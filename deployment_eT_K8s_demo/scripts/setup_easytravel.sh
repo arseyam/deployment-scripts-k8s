7351c6d1897777687b56e8f5778078057d18cb39
Shaun Gautz <shaun.gautz@dynatrace.com>
2019-08-22 11:10:26 -0500

    Delete setup_easytravel.sh

--- deployment_eT_K8s_demo/scripts/setup_easytravel.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-echo "Running setup"
-#!./setup_base.sh
-
-kubectl create namespace easyTravel
-#!kubectl config use-context demo-live-1
-#!kubectl config set contexts.default-context.namespace demo-live-1
