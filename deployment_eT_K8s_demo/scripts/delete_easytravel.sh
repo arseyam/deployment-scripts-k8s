ac4a1e35f6d756c8181de9b7b0d3d32c00525cd7
Shaun Gautz <shaun.gautz@dynatrace.com>
2019-08-22 11:11:09 -0500

    Update delete_easytravel.sh

--- deployment_eT_K8s_demo/scripts/delete_easytravel.sh
+++ deployment_eT_K8s_demo/scripts/delete_easytravel.sh
@@ -1,11 +1,4 @@
 #!/bin/sh
 
 echo "Deleting easyTravel"
-./setup_demo1.sh
-./delete.sh
-
-echo "Deleting exposure and traffic generator"
-kubectl delete -f easytravel-loadgen-deployment.yaml
-kubectl delete -f easytravel-frontend-service.yaml
-kubectl delete -f easytravel-backend-service.yaml
-
+kubectl delete namespace easyTravel
