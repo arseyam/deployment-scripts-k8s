08d3166fe4af3893b6225f10ca53dd33879ef708
Shaun Gautz <shaun.gautz@dynatrace.com>
2019-08-22 11:03:31 -0500

    Update and rename demo1_create.sh to create_easytravel.sh

---
+++ deployment_eT_K8s_demo/scripts/create_easytravel.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+echo "Creating easyTravel"
+./setup_demo1.sh
+./create.sh
+
+echo "Exposing easyTravel"
+kubectl create -f easytravel-backend-service.yaml
+kubectl create -f easytravel-frontend-service.yaml
+echo "Waiting for 150 seconds before starting loadgen"
+sleep 150
+kubectl create -f easytravel-loadgen-deployment.yaml
